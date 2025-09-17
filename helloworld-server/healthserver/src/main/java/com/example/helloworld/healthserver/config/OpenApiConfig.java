32ad58b85563c2cd0331a0a78a2d5b8f9f5aa573
송진우 <rkddkwl059@naver.com>
2025-09-17 15:40:18 +0900

    !hotfix : swagger url fixed

---
+++ helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/config/OpenApiConfig.java
@@ -0,0 +1,13 @@
+package com.example.helloworld.healthserver.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public io.swagger.v3.oas.models.OpenAPI openAPI() {
+        return new io.swagger.v3.oas.models.OpenAPI()
+                .servers(java.util.List.of(new io.swagger.v3.oas.models.servers.Server().url("/user")));
+    }
+}
