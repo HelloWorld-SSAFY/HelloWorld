3813670053d4e9950dd70bb765287a74c76a5b7f
정보균 <ototkk58@naver.com>
2025-09-18 19:39:05 +0900

    Revert "Fix: feign 관련 파일 삭제"
    This reverts commit 34c6c7eef1c3181579e8bec433b30e375868c0f4.

---
+++ helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/config/UserServerClient.java
@@ -0,0 +1,16 @@
+package com.example.helloworld.healthserver.config;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient(
+        name = "user-server",                 // Eureka 쓰면 name만
+        url = "${userserver.base-url:}",      // 정적 주소면 이 값 사용
+        configuration = FeignAuthConfig.class
+)
+public interface UserServerClient {
+    @GetMapping("/api/users/me")
+    MeResponse me();
+
+    record MeResponse(Long userId, Long coupleId, String email) {}
+}
