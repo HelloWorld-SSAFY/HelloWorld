401cd2de1bedf6181b03bb454452062201a073da
송진우 <rkddkwl059@naver.com>
2025-09-22 09:14:58 +0900

    feat(S13P21D204-163) : weekly swagger authorize fixed

--- helloworld-server/weeklyserver/src/main/java/com/example/helloworld/weeklyserver/config/OpenApiConfig.java
+++ helloworld-server/weeklyserver/src/main/java/com/example/helloworld/weeklyserver/config/OpenApiConfig.java
@@ -5,9 +5,27 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
+
     @Bean
     public io.swagger.v3.oas.models.OpenAPI openAPI() {
+        final String schemeName = "bearerAuth";
+
         return new io.swagger.v3.oas.models.OpenAPI()
-                .servers(java.util.List.of(new io.swagger.v3.oas.models.servers.Server().url("/weekly")));
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("weekly API")
+                        .version("v1")
+                        .description("Weekly API"))
+                // Gateway 아래로 붙는 베이스 경로 지정 (예: /user)
+                .servers(java.util.List.of(new io.swagger.v3.oas.models.servers.Server().url("/weekly")))
+                // 보안 스키마 + 글로벌 적용
+                .components(new io.swagger.v3.oas.models.Components().addSecuritySchemes(
+                        schemeName,
+                        new io.swagger.v3.oas.models.security.SecurityScheme()
+                                .name(schemeName)
+                                .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                ))
+                .addSecurityItem(new io.swagger.v3.oas.models.security.SecurityRequirement().addList(schemeName));
     }
 }
