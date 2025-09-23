9e7719896a9f4cacc4838a9a9fd29dfd5ec4b69f
정보균 <ototkk58@naver.com>
2025-09-23 16:31:38 +0900

    feat: 필터에 order 추가해서 가장 먼저 실행되도록 함.

--- helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/config/UserInfoAuthenticationFilter.java
+++ helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/config/UserInfoAuthenticationFilter.java
@@ -7,6 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -17,6 +18,7 @@ import java.io.IOException;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Order(0)
 public class UserInfoAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
