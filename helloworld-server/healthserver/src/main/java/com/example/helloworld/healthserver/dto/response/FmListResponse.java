32eb465f218ba6e7ed7ad012efb62aa87cf9a3c2
정보균 <ototkk58@naver.com>
2025-09-24 22:21:07 +0900

    Fix: 서울 기준으로 기록 시간 저장

--- helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/dto/response/FmListResponse.java
+++ helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/dto/response/FmListResponse.java
@@ -1,13 +1,13 @@
 package com.example.helloworld.healthserver.dto.response;
 
-import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 
 public record FmListResponse(
         List<FmListItem> records
 ) {
     public record FmListItem(
-            Instant recorded_at, // 해당 '일'의 시작 시각(Asia/Seoul 기준 00:00)을 UTC로 변환한 값
+            LocalDate recorded_at,
             int total_count
     ){}
 }
