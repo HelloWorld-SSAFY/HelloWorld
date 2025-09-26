60e29eb941bf8c005c6765603c7633fe35c51817
정보균 <ototkk58@naver.com>
2025-09-26 21:51:06 +0900

    Fix: 헬스서버 Nofitication_recipients 제거

--- helloworld-server/healthserver/src/main/java/com/example/helloworld/healthserver/alarm/repository/NotificationRecipientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.helloworld.healthserver.alarm.repository;
-
-import com.example.helloworld.healthserver.alarm.entity.NotificationRecipient;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NotificationRecipientRepository extends JpaRepository<NotificationRecipient, Long> {}
